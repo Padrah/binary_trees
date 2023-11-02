af1cda218cf8d0ce381be1399818a70630f7a1ff
Patrick <pchipungu50@gmail.com>
2023-11-03 00:20:44 +0200

    fix2

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,6 +2,9 @@
 
 /**
  * binary_tree_node - Create a binary node
+ * @parent: node parent
+ * @value: Value of the node
+ * Return: the new node
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - free an entire tree using recursion it free the node
+ * free an entire tree using recursion it free the node
+ * when the node left and right are NULL
+ * @tree: tree to free
+ * Return: Nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
